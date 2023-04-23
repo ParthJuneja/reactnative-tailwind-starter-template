f3228bb45ad824f69178da1fa9704e55990ea4b4
parth <admin@test.com>
2023-04-24 02:19:22 +0530

    init w nativewind

---
+++ screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import {
+  View,
+  Image,
+  SafeAreaView,
+  Text,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="bg-white pt-5">
+      <View>
+        <Text>Hellow World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
